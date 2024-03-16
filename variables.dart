2bcf6d0402ecc1c4cf2d54495306a7b31047100f
TONYYAEL <160343221+TONYYAEL@users.noreply.github.com>
2024-03-16 19:26:10 +0300

    Add files via upload

---
+++ variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Define variables
+  String name = 'ANTONY KARANJA';
+  int age = 20;
+  String school = 'Plp';
+  String hobby = 'Cycling';
+
+  // Print information in one sentence
+  print(
+      'My name is $name, I am $age years old, and I attend $school. My hobby is $hobby.');
+}
